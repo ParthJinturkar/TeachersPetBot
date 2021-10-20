9b5cccae99b170d09ba4f36b064f53c853e49235
wevanbrown <wevanbrown@gmail.com>
2021-10-20 18:18:59 -0400

    Added conftest in preparation for testing

---
+++ conftest.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+import discord.ext.test as dpytest
+import pytest
+from discord import Intents
+from discord.ext.commands import Bot
+from setuptools import glob
+from os.path import dirname as d
+from os.path import abspath, join
+
+intents = Intents.all()
+
+root_dir = d(d(abspath("test/test_bot.py")))
+sys.path.append(root_dir)
+
+# Default parameters for the simulated dpytest bot. Loads the bot with commands from the /cogs directory
+# Ran everytime pytest is called
+@pytest.fixture
+def bot(event_loop):
+    bot = Bot(intents=intents, command_prefix="!", loop=event_loop)
+    dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(dir)
+    os.chdir('cogs')
+    for filename in os.listdir(os.getcwd()):
+        if filename.endswith('.py'):
+            bot.load_extension(f"cogs.{filename[:-3]}")
+    dpytest.configure(bot)
+    return bot
+
+
+# Cleans up leftover files generated through dpytest
+def pytest_sessionfinish():
+    # Clean up attachment files
+    files = glob.glob('./dpytest_*.dat')
+    for path in files:
+        try:
+            os.remove(path)
+        except Exception as e:
+            print(f"Error while deleting file {path}: {e}")
+    print("\npySession closed successfully")
+
+# Copyright (c) 2021 War-Keeper
