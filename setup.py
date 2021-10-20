4a3fb545979773dd9d76d5bd1d8b0f712971dc48
wevanbrown <wevanbrown@gmail.com>
2021-10-20 18:22:33 -0400

    Added setup.py just for documentation

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='TeachersPetBot',
+    version='1.0',
+    description='A Discord bot for classroom discord channels',
+    long_description=""" A modification of TeachersPetBot bot which provides utility commands for students and teachers in classrom discord channels """,
+    author='Chaitanya Patel, Walter Evan Brown, Kunwar Vidhan, Sumedh Sanjay Salvi, Sunil Dattatraya Upare',
+    author_emails='cpatel3@ncsu.edu, webrown2@ncsu.edu, kvidhan@ncsu.edu, ssalvi@ncsu.edu, supare@ncsu.edu',
+    url='https://github.com/War-Keeper/TeachersPetBot',
+    liscense='MIT',
+    install_requires=['pytest', 'discord.py', 'dpytest'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3.9'
+                 ]
+)
+
+# Copyright (c) 2021 War-Keeper
