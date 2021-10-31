f8e04c62a7cefe82bed22e434337e38bff5e79b6
supare <supare@ncsu.edu>
2021-10-31 16:13:18 -0400

    Restricted multiple reactions on single poll

--- cogs/polling.py
+++ cogs/polling.py
@@ -40,7 +40,30 @@ class Helper(commands.Cog):
         await msg.add_reaction('👍')
         await msg.add_reaction('👎')
 
-
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        user = payload.member
+        if user.bot:return
+        msg = await self.bot.get_guild(payload.guild_id).get_channel(payload.channel_id).fetch_message(payload.message_id)
+        emoji = payload.emoji
+        users = []
+        if msg.author.bot and("👍"and"👎")in[str(i)for i in msg.reactions]:
+            for react in msg.reactions:
+                if str(react)=="👍":
+                    async for reactor in react.users():
+                        if reactor.bot:continue
+                        if reactor in users:
+                            await msg.remove_reaction(emoji, user)
+                            return
+                        users.append(reactor)
+                elif str(react)=="👎":
+                    async for reactor in react.users():
+                        if reactor.bot:continue
+                        if reactor in users:
+                            await msg.remove_reaction(emoji, user)
+                            return
+                    return
+   
 
 
 # --------------------------------------
