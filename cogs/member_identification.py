3219f711a5fdba9f6d4d793eeb412cf116b05e85
supare <supare@ncsu.edu>
2021-10-31 20:09:47 -0400

    Added error handling in member identification command

--- cogs/member_identification.py
+++ cogs/member_identification.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import datetime
 
 from discord.ext.commands import bot
 # ---------------------------------------------------------------------------------------
@@ -14,20 +13,34 @@ class Helper(commands.Cog):
     @commands.command(name = "whois")
     @commands.has_role("Instructor")
     async def identify_member(self, ctx, *, member_name: str = None):
-        member = ctx.guild.get_member_named(member_name)
-        roles = [role for role in member.roles]
-        embed = discord.Embed(colour=discord.Colour.orange(), timestamp=ctx.message.created_at,
-                              title=str(member))
-        embed.set_thumbnail(url=member.avatar_url)
-        embed.set_footer(text=f"Requested by {ctx.author}")
-        embed.add_field(name="Display Name:", value=member.display_name)
-        embed.add_field(name="ID:", value=member.id)
-        embed.add_field(name="Created Account On:", value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
-        embed.add_field(name="Joined Server On:", value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
-        embed.add_field(name="Roles:", value="".join([role.mention for role in roles[1:]]))
-        embed.add_field(name="Highest Role:", value=member.top_role.mention)
-        await ctx.send(embed=embed)
-        
+        msg = ctx.message.content
+        await ctx.message.delete()
+        if ctx.channel.name == 'instructor-commands':
+            if member_name == None:
+                embed = discord.Embed(description="!whois command should be used in following way:\n\n`!whois member_username`", color=discord.colour.Color.red())
+                await ctx.author.send(embed=embed)
+                return
+            member = ctx.guild.get_member_named(member_name)
+            if member:
+                roles = [role for role in member.roles]
+                embed = discord.Embed(colour=discord.Colour.orange(), timestamp=ctx.message.created_at,
+                                    title=str(member))
+                embed.set_thumbnail(url=member.avatar_url)
+                embed.set_footer(text=f"Requested by {ctx.author}")
+                embed.add_field(name="Display Name:", value=member.display_name)
+                embed.add_field(name="ID:", value=member.id)
+                embed.add_field(name="Created Account On:", value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
+                embed.add_field(name="Joined Server On:", value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
+                embed.add_field(name="Roles:", value="".join([role.mention for role in roles[1:]]))
+                embed.add_field(name="Highest Role:", value=member.top_role.mention)
+                await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(description="!whois could not find member with username\n\n`{member_name}`", color=discord.colour.Color.red())
+                await ctx.author.send(embed=embed)
+        else:
+            embed=discord.Embed(description="`!whois` can only be used in the `instructor-commands` channel.\n\nYou entered the following command:\n\n`" + msg + "`", color=discord.colour.Color.red())
+            await ctx.author.send(embed=embed)         
+
 # --------------------------------------
 # add the file to the bot's cog system
 # --------------------------------------
