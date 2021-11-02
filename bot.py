7625dd61b6bb71fb267792741cab305a614412e0
Kunwar Vidhan <51852048+kunwarvidhan@users.noreply.github.com>
2021-11-01 20:34:11 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -155,6 +155,22 @@ async def shutdown(ctx):
     db.delete_db()
     exit()
 
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if after.channel != None:
+        if after.channel.id == 897659220365836368:
+            for guild in bot.guilds:
+                maincategory = discord.utils.get(
+                    guild.categories, id=897659220365836366)
+                channel2 = guild.create_voice_channel(name=f'канал {member.display_name}', category=maincategory)
+                await channel2.set_permissions(member, connect=True, mute_members=True, manage_channels=True)
+                await member.move_to(channel2)
+
+                def check(x, y, z):
+                    return len(channel2.members) == 0
+                await bot.wait_for('voice_state_update', check=check)
+                await channel2.delete()
+
 
 ''' run bot command '''
 bot.run(TOKEN)
