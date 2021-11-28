e6f63dbb7644b5f939c0b9020b1b788a31ec1ce8
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-11-28 12:19:24 -0500

    Add check funtion

--- cogs/take.py
+++ cogs/take.py
@@ -15,6 +15,7 @@ class Create(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     @commands.command(name='importevents', help='Create a create events from csv file.')
     # @commands.dm_only()
     @commands.has_role('Instructor')
@@ -26,14 +27,16 @@ class Create(commands.Cog):
             if not os.path.exists(temp):
                 os.makedirs(temp)
 
+            def check(m):
+                return m.content is not None and m.author == ctx.author
+
             # Ask for a file if there are no attachments in the initital message
             if len(ctx.message.attachments) == 0:
                 await ctx.send("Please upload your file below.")
 
                 # Loops until we receive a file.
                 while True:
-
-                    event_msg = await self.bot.wait_for("message")
+                    event_msg = await self.bot.wait_for("message", check=check)
 
                     if len(event_msg.attachments) != 1:
                         await ctx.send(
