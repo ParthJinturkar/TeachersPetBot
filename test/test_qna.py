10c181b520c4682378a44efd221cdfeece791623
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-11-29 14:06:57 -0500

    Added test_question Q&A tests

--- test/test_qna.py
+++ test/test_qna.py
@@ -1,3 +1,6 @@
+import discord.ext.test as dpytest
+import pytest
+
 ###########################
 # Tests Q-and-A functionality
 ###########################
@@ -6,37 +9,47 @@ from time import sleep
 
 
 ###########################
-# Function: test
-# Description: runs each test
-# Inputs:
-#      - testing_bot: bot that sends commands to test TeachersPetBot
-#      - guild_id: id of the guild that is using the TeachersPetBot
-# Outputs: None
-###########################
-async def test(testing_bot, guild_id):
-    await test_question(testing_bot)
-    await test_answer(testing_bot, guild_id)
-    await test_instr_answer(testing_bot, guild_id)
-
-
-###########################
 # Function: test_question
 # Description: tests questioning functionality
 # Inputs:
-#      - testing_bot: bot that sends commands to test TeachersPetBot
+#      - bot: bot that sends commands to test TeachersPetBot
 # Outputs: None
 ###########################
-async def test_question(testing_bot):
-    print('testing question')
-    qna_channel = discord.utils.get(testing_bot.get_all_channels(), name='q-and-a')
-    await qna_channel.send('!ask \"Hello\"')
+@pytest.mark.asyncio
+async def test_question(bot):
+    await dpytest.message('!ask \"Hello\"')
 
-    sleep(0.5)
+    sleep(.5)
 
-    messages = await qna_channel.history(limit=1).flatten()
-    for m in messages:
-        assert 'Q1: Hello' in m.content
+###########################
+# Function: test_question_no_input
+# Description: tests questioning functionality when given incorrect input
+# Inputs:
+#      - bot: bot that sends commands to test TeachersPetBot
+# Outputs: None
+###########################
+@pytest.mark.asyncio
+async def test_question_no_input(bot):
+    try:
+        await dpytest.message('!ask')
+    except discord.ext.commands.errors.MissingRequiredArgument:
+        return True
+    assert False
 
+###########################
+# Function: test_question_invalid
+# Description: tests questioning functionality when given incorrect input
+# Inputs:
+#      - bot: bot that sends commands to test TeachersPetBot
+# Outputs: None
+###########################
+@pytest.mark.asyncio
+async def test_question_invalid(bot):
+    try:
+        await dpytest.message('!ask\"hello\"')
+    except discord.ext.commands.errors.CommandNotFound:
+        return True
+    assert False
 
 ###########################
 # Function: test_answer
@@ -46,18 +59,19 @@ async def test_question(testing_bot):
 #      - guild_id: id of the guild that is using the TeachersPetBot
 # Outputs: None
 ###########################
-async def test_answer(testing_bot, guild_id):
+@pytest.mark.asyncio
+async def test_answer(bot):
     print('testing answer')
 
     # make sure bot is not an instructor
-    guild = testing_bot.get_guild(guild_id)
+    guild = bot.get_guild("test")
     role = discord.utils.get(guild.roles, name="Instructor")
-    member = guild.get_member(testing_bot.user.id)
+    member = guild.get_member(bot.user.id)
     if "instructor" in [y.name.lower() for y in member.roles]:
         await member.remove_roles(role)
 
-    qna_channel = discord.utils.get(testing_bot.get_all_channels(), name='q-and-a')
-    await qna_channel.send('!answer 1 \"World\"')
+    qna_channel = discord.utils.get(bot.get_all_channels(), name='q-and-a')
+    await dpytest.message('!answer 1 \"World\"')
 
     sleep(1.5)
 
@@ -84,7 +98,7 @@ async def test_instr_answer(testing_bot, guild_id):
 
     # answer question as instructor
     qna_channel = discord.utils.get(testing_bot.get_all_channels(), name='q-and-a')
-    await qna_channel.send('!answer 1 \"Hello World\"')
+    await dpytest.message('!answer 1 \"Hello World\"')
 
     sleep(1.5)
 
