f1462ab2a59f5a0a2c590b97cf2541fa9e75c232
Parth <parth.jinturkar3660@gmail.com>
2021-11-29 21:56:53 -0500

    updated all docstrings in ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -10,16 +10,19 @@ class Helpful(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # -------------------------------------------------------------------------------------------------------
-    #    Function: ping(self, ctx)
-    #    Description: prints the current ping of the bot, used as a test function
-    #    Inputs:
-    #    - self: used to access parameters passed to the class through the constructor
-    #    - ctx: used to access the values passed through the current context
-    #    Outputs: prints the current ping of the bot, with an upper bound of 999999999 to avoid float errors
-    # -------------------------------------------------------------------------------------------------------
     @commands.command()
     async def ping(self, ctx):
+        """
+        Function:
+           ping(self, ctx)
+        Description:
+           prints the current ping of the bot, used as a test function
+        Inputs:
+            - self: used to access parameters passed to the class through the constructor
+            - ctx: used to access the values passed through the current context
+        Outputs:
+            prints the current ping of the bot, with an upper bound of 999999999 to avoid float errors
+        """
         # We set an upper bound on the ping of the bot to prevent float_infinity situations which crash testing
         await ctx.send(f"Pong! My ping currently is {round(min(999999999, self.bot.latency * 1000))}ms")
 
