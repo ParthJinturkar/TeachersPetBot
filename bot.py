7ce2580bf22bc35974358dc24386a54ce18e3d9f
Parth <parth.jinturkar3660@gmail.com>
2021-11-29 21:48:32 -0500

    updated all docstrings in bot.py

--- bot.py
+++ bot.py
@@ -140,16 +140,24 @@ async def on_message_edit(before, after):
 
 
 ############################
-#    Function: shutdown(ctx)
+#    Function:
 #    Description: Command for shutting down the bot
-#    Inputs: used to access the values passed through the current context
-#    Outputs:
-#           sends a message after closing the bot
+#    Inputs:
+#
 # ###########################
 @bot.command(name="shutdown", help="Shuts down the bot, only usable by the owner")
 @commands.has_permissions(administrator=True)
 async def shutdown(ctx):
-    ''' Command for shutting down the bot '''
+    """
+    Function:
+        shutdown(ctx)
+    Description:
+        Command for shutting down the bot
+    Inputs:
+        used to access the values passed through the current context
+    Outputs:
+        sends a message after closing the bot
+    """
     db.shutdown()
     await ctx.send('Shutting Down bot')
     print("Bot closed successfully")
@@ -157,16 +165,15 @@ async def shutdown(ctx):
     db.delete_db()
     exit()
 
-############################
-#    Function: start_bot(guild)
-#    Description: run when the bot starts or when a new guild is added
-#    Inputs:
-#    - guild : the guild the bot is added to
-#    Outputs:
-#     -
-# ###########################
 async def start_bot(guild):
-    ''' run when the bot starts or when a new guild is added '''
+    """
+    Function:
+        start_bot(guild)
+    Description:
+        run when the bot starts or when a new guild is added
+    Inputs:
+        - guild : the guild the bot is added to
+    """
     print("Bot is now online")
     check = False
 
@@ -200,16 +207,18 @@ async def start_bot(guild):
         await guild.create_text_channel('q-and-a')
 
 
-###########################
-#    Function: create_voice_channels()
-#    Description: run when the bot starts or when a new guild is added
-#    Inputs:
-#    - guild to create voice channels in
-#    Outputs:
-#     - create voice channels with limits for the number of users in that channel
-###########################
 async def create_voice_channels(guild):
-    ''' run on bot startup '''
+    """
+    Function:
+        create_voice_channels()
+    Description:
+        run when the bot starts or when a new guild is added
+    Inputs:
+        - guild to create voice channels in
+    Outputs:
+        - create voice channels with limits for the number of users in that channel
+    """
+
     # for cat in guild.categories:
     #     if cat.name == 'General Office Hours' or cat.name == 'Teams':
     #         return
