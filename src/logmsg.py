4a596b45eabb2cef2bf9459fbd8f313a9a606958
yrajapa <yasitha.rajapaksha@gmail.com>
2021-11-29 23:17:16 -0500

    Style fixes

--- src/logmsg.py
+++ src/logmsg.py
@@ -1,15 +1,13 @@
 import logging
-import os
 from datetime import date
+
 
 def get_today():
     today = date.today()
     dt_string = today.strftime("%d_%m_%Y")
     return dt_string
 
+
 def logerror(error):
-
     logging.basicConfig(level=logging.DEBUG)
-
-    logging.info(error);
-
+    logging.info(error)
